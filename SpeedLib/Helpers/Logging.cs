06ce8fa539ffba839232de6836d8248aa451909f
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2024-01-18 08:40:30 +0000

    add logging

---
+++ SpeedLib/Helpers/Logging.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SpeedLib.SpeedLib.Helpers
+{
+    internal static class Logging
+    {
+        public static void Log(this Exception e) => Svc.Log.Error($"{e.Message}\n{e.StackTrace ?? ""}");
+
+        public static void Log(this Exception e, string ErrorMessage) => Svc.Log.Error($"{ErrorMessage}\n{e.Message}\n{e.StackTrace ?? ""}");
+    }
+}
